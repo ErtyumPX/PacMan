a9f37922c9072b26da5aadc771039a7315ecf98a
ErtyumPX <ertyum35@gmail.com>
2022-06-11 18:28:41 +0300

    modified map_creater to place spawn and berries

--- Game/map_creater.py
+++ Game/map_creater.py
@@ -27,6 +27,11 @@ def clear_tiles():
     for _ in range(H_TILES):
         column = [2 for _ in range(V_TILES)]
         TILES.append(column)
+    global SUPER_BERRIES, main_pacman_position
+    for super_berry in SUPER_BERRIES:
+        del super_berry
+    SUPER_BERRIES = []
+    main_pacman_position = [-1, -1]
     print("Cleared")
 
 
@@ -51,10 +56,12 @@ def open_func():
     except Exception as exp:
         print(exp)
 
+SUPER_BERRIES = []
+main_pacman_position = [-1, -1]
 
 clear_tiles()
 
-mouse_status = 0
+tile_change_status = None
 
 #button_image = pygame.transform.scale(pygame.image.load("data/white.jpg"), (60,20))
 open_button = TextButton(surface, x=650, y=40, width=100, height=24, text="Open Map", font_size=12, func=open_func)
@@ -74,29 +81,54 @@ while main:
     surface.fill((255, 102, 102))
     EVENTS = pygame.event.get()
     PRESSED_KEYS = pygame.key.get_pressed()
-    for event in EVENTS:
-        if event.type == pygame.QUIT:
-            main = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_status = 1
-            elif event.button == 3:
-                mouse_status = 2
-            elif event.button == 2:
-                mouse_status = 3
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouse_status = 0
 
     mouse_pos = pygame.mouse.get_pos()
     mouse_tile_x = mouse_pos[0] // TILE_WIDTH
     mouse_tile_y = mouse_pos[1] // TILE_WIDTH
 
+    for event in EVENTS:
+        if event.type == pygame.QUIT:
+            main = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                tile_change_status = 1
+            elif event.button == 3:
+                tile_change_status = 2
+            elif event.button == 2:
+                tile_change_status = 3
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            tile_change_status = None
+
+        elif event.type == pygame.KEYDOWN:
+            if -1 < mouse_tile_x < H_TILES and -1 < mouse_tile_y < V_TILES:
+                tile_position = [mouse_tile_x * TILE_WIDTH + TILE_WIDTH/2, mouse_tile_y * TILE_WIDTH + TILE_WIDTH/2]
+
+                if event.key == pygame.K_SPACE:
+                    if main_pacman_position == tile_position:
+                        main_pacman_position = [-1, -1]
+                    else:
+                        TILES[mouse_tile_x][mouse_tile_y] = 0
+                        main_pacman_position = tile_position 
+                
+                elif event.key == pygame.K_f:
+                    if tile_position in SUPER_BERRIES:
+                        SUPER_BERRIES.remove(tile_position)
+                    else:
+                        SUPER_BERRIES.append(tile_position)
+                        TILES[mouse_tile_x][mouse_tile_y] = -1
+
+        elif event.type == pygame.KEYUP:
+            tile_change_status = None
+
+
     if -1 < mouse_tile_x < H_TILES and -1 < mouse_tile_y < V_TILES:
-        if mouse_status == 1:
+        if tile_change_status == 1:
             TILES[mouse_tile_x][mouse_tile_y] = 1
-        elif mouse_status == 2:
+        elif tile_change_status == 2:
             TILES[mouse_tile_x][mouse_tile_y] = 2
-        elif mouse_status == 3:
+        elif tile_change_status == 3:
             TILES[mouse_tile_x][mouse_tile_y] = 3
 
 
@@ -106,7 +138,7 @@ while main:
         for y in range(defaults.V_TILES):
             tile = TILES[x][y]
             color = (0, 0, 0)
-            if tile == 1: color = defaults.ACTIVE_COLOR
+            if tile == 1 or tile == 0 or tile == -1: color = defaults.ACTIVE_COLOR
             elif tile == 2: color = defaults.OBSTACLE_COLOR
             elif tile == 3: color = defaults.INACTIVE_COLOR
             pygame.draw.rect(surface, color, pos)
@@ -123,6 +155,9 @@ while main:
 
     ProcessElements(EVENTS, PRESSED_KEYS, mouse_pos, BUTTONS, INPUT_BOXES, TEXTS)
     UpdateElements(BUTTONS, INPUT_BOXES, TEXTS)
+    for berry_position in SUPER_BERRIES:
+        pygame.draw.circle(surface, (0, 0, 0), berry_position, 5)
+    pygame.draw.circle(surface, (255, 255, 0), main_pacman_position, 7)
     pygame.display.update()
 
 pygame.quit()
